0f24aafab7cde138fd13eb801bc6aa890a6b8d69
Williano <paawilly17@gmail.com>
2020-10-10 20:04:41 -0500

    Refactor

--- Python/arrays/python_array/arrays.py
+++ Python/arrays/python_array/arrays.py
@@ -8,6 +8,9 @@ from array_iterator import ArrayIterator
 class Array(object):
 
     def __init__(self, size):
+        """
+            Implements the array data structure in Python
+        """
         assert size > 0, "ERROR!!! Array size must be greater than zero."
 
         self.array_size = size
@@ -19,23 +22,38 @@ class Array(object):
 
 
     def __len__(self):
+        """
+            Returns the lenght of the array
+        """
         return self.array_size
 
 
     def __get_item__(self, index):
+        """
+            Returns an element from a specific location
+        """
         assert index >= 0 and index < len(self), "ERROR!!! Array subscript out of range."
         return self._elements[index]
 
 
     def __set_item__(self, index, value):
+        """
+            Sets an element to a specific position
+        """
         assert index >= 0 and index < len(self), "ERROR!!! Array subscript out of range."
         self._elements[index] = value
 
 
     def initialize_array(self, value):
+        """
+            Initializes the array with specified value
+        """
         for index in range(len(self)):
             self._elements[index] = value
 
 
     def __iter__(self):
+        """
+            Iterates through the array and returns all elements
+        """
         return _ArrayIterator(self._elements)
