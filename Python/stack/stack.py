c7e8f815f90f0677c577a2be1d304ff9c6852994
Williano <paawilly17@gmail.com>
2020-12-12 19:35:32 -0600

    Add stack class

---
+++ Python/stack/stack.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+class Stack(object):
+
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        self.stack.pop()
+
+    def peek(self):
+        self.stack[-1]
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
