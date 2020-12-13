ca61f2852199c21440bf85084178307351631309
Williano <paawilly17@gmail.com>
2020-12-12 20:19:49 -0600

    Add example of stack implementation

--- Python/stack/stack_example.py
+++ Python/stack/stack_example.py
@@ -1,16 +1,24 @@
 from stack.python_stack import Stack
 
 
-def main():
+def reverse_string(sentence):
 
     stack_container = Stack()
 
-    print("Pushing....")
-    stack_container.push(5)
+    splitted_sentence = sentence.split(" ")
 
-    print("")
+    for word in splitted_sentence:
+        stack_container.push(word[::-1])
 
-    print(stack_container.peek())
+    for word in range(len(splitted_sentence)):
+        print(stack_container.pop())
+
+
+def main():
+
+    sentence = "We will conquere COVID-19"
+
+    reverse_string(sentence)
 
 
 if __name__ == "__main__":
