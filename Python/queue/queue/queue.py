b199160fd806441a5f5c28f274a3c8acc6209281
Williano <paawilly17@gmail.com>
2020-12-19 22:58:28 -0600

    Add methods

---
+++ Python/queue/queue/queue.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+class Queue(object):
+
+    def __init__(self):
+        self._queue = deque()
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def enqueue(self):
+        pass
+
+    def deque(self):
+        pass
+
+
