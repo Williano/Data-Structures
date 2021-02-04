d3691a8179a23833987493a25477c91c3ee6df98
Williano <paawilly17@gmail.com>
2021-02-04 09:41:47 -0600

    Add method to insert at front

--- Python/linked_list/linked_list/linked_list.py
+++ Python/linked_list/linked_list/linked_list.py
@@ -1,3 +1,12 @@
+from .node import Node
+
+
 class LinkedList(object):
     def __init__(self):
         self.head = None
+
+    def add_at_begining(self, data):
+        node = Node(data=data, next=self.head)
+        self.head = node
+
+
