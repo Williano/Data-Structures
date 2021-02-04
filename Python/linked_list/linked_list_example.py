35d4462bb17c3a5ea10945580e7385944bfaca62
Williano <paawilly17@gmail.com>
2021-02-04 10:46:10 -0600

    Update

--- Python/linked_list/linked_list_example.py
+++ Python/linked_list/linked_list_example.py
@@ -5,10 +5,26 @@ def main():
 
     linked_list = LinkedList()
 
-    linked_list.add_at_begining(5)
-    linked_list.add_at_begining(20)
+    linked_list.insert_at_front(5)
+    linked_list.insert_at_front(20)
     linked_list.insert_at_end(50)
 
+    linked_list.traverse_list()
+
+    print()
+    print("Inserting list")
+    linked_list.insert_list_at_end(["Will", "Bill", "Me"])
+    print()
+    print("Printing list")
+    print()
+    linked_list.traverse_list()
+
+    print()
+    print("Inserting list")
+    linked_list.insert_list_at_front(["Will", "Bill", "Me"])
+    print()
+    print("Printing list")
+    print()
     linked_list.traverse_list()
 
 
