57777aa981347812c227d879acb507cf95f87a04
Williano <paawilly17@gmail.com>
2021-02-04 09:48:44 -0600

    Add example to test link list add and traverse

--- Python/linked_list/linked_list_example.py
+++ Python/linked_list/linked_list_example.py
@@ -5,6 +5,11 @@ def main():
 
     linked_list = LinkedList()
 
+    linked_list.add_at_begining(5)
+    linked_list.add_at_begining(20)
+
+    linked_list.traverse_list()
+
 
 if __name__ == "__main__":
     main()
