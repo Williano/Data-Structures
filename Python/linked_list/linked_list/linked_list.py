733f3bc43abb7eb1ae7aa2f6e5f7e67bc5028098
Williano <paawilly17@gmail.com>
2021-02-04 10:37:00 -0600

    Add method to insert list of values

--- Python/linked_list/linked_list/linked_list.py
+++ Python/linked_list/linked_list/linked_list.py
@@ -27,3 +27,9 @@ class LinkedList(object):
                 current_node = current_node.next
 
             current_node.next = Node(data=data, next=None)
+
+    def insert_list(self, data_list):
+        self.head = None
+
+        for data in data_list:
+            self.insert_at_end(data)
