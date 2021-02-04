f73caa854fadb3a2b8edea57cc46226ef78fd4b3
Williano <paawilly17@gmail.com>
2021-02-04 10:13:46 -0600

    Refactor

--- Python/linked_list/linked_list/linked_list.py
+++ Python/linked_list/linked_list/linked_list.py
@@ -5,13 +5,13 @@ class LinkedList(object):
     def __init__(self):
         self.head = None
 
-    def add_at_begining(self, data):
-        node = Node(data=data, next=self.head)
-        self.head = node
-
     def traverse_list(self):
         current_node = self.head
 
         while current_node is not None:
             print(current_node.data)
             current_node = current_node.next
+
+    def add_at_begining(self, data):
+        node = Node(data=data, next=self.head)
+        self.head = node
