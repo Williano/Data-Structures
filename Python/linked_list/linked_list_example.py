b8ba95f18242b4cdb86577790a80b91c0ac26f60
Williano <paawilly17@gmail.com>
2021-02-06 11:50:34 -0600

    Fix bug

--- Python/linked_list/linked_list_example.py
+++ Python/linked_list/linked_list_example.py
@@ -5,6 +5,7 @@ def main():
 
     linked_list = LinkedList()
 
+    # Insert at front
     linked_list.insert_at_front(5)
     linked_list.insert_at_front(20)
     linked_list.insert_at_end(50)
@@ -14,6 +15,8 @@ def main():
 
     print()
     print("Inserting list")
+
+    # Insert list at end
     linked_list.insert_list_at_end(["Will", "Bill", "Me"])
     print(linked_list.size_of_list())
     print()
@@ -21,9 +24,21 @@ def main():
     print()
     linked_list.traverse_list()
 
+    # Insert list the front
     print()
     print("Inserting list")
     linked_list.insert_list_at_front(["Will", "Bill", "Me"])
+    print(linked_list.size_of_list())
+    print()
+    print("Printing list")
+    print()
+    linked_list.traverse_list()
+
+    # Remove list a specific index
+    print()
+    print("Inserting list")
+    linked_list.insert_list_at_front(["Test", "Example", "Test"])
+    linked_list.remove_at_index(2)
     print(linked_list.size_of_list())
     print()
     print("Printing list")
