6b99eee6ab2df2f1f82045da01a2056b960e4517
Williano <paawilly17@gmail.com>
2021-02-06 13:11:51 -0600

    Refactor

---
+++ Python/linked_list/linked_list/singly_linked_list/singly_linked_list.py
@@ -0,0 +1,99 @@
+from .singly_node import SinglyNode
+
+
+class SinglyLinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def size_of_list(self):
+        count = 0
+        current_node = self.head
+
+        while current_node:
+            count += 1
+            current_node = current_node.next
+
+        return count
+
+    def traverse_list(self):
+        current_node = self.head
+
+        while current_node is not None:
+            print(current_node.data, end=" ---> ")
+            current_node = current_node.next
+
+    def insert_at_front(self, data):
+        node = SinglyNode(data=data, next=self.head)
+        self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = SinglyNode(data=data, next=None)
+            return
+        else:
+            current_node = self.head
+
+            while current_node.next:
+                current_node = current_node.next
+
+            current_node.next = SinglyNode(data=data, next=None)
+
+    def insert_at_index(self, index, data):
+
+        count = 0
+
+        self.check_index_out_of_range(index)
+
+        if index == 0:
+            self.insert_at_front(data)
+            return
+
+        current_node = self.head
+        while current_node:
+            if count == index - 1:
+                node = SinglyNode(data=data, next=current_node.next)
+                current_node.next = node
+
+            current_node = current_node.next
+            count += 1
+
+    def insert_list_at_front(self, data_list):
+        self.head = None
+
+        for data in data_list:
+            self.insert_at_front(data)
+
+    def insert_list_at_end(self, data_list):
+        self.head = None
+
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def insert_after_value(self, data_after, data_to_insert):
+        pass
+
+    def remove_by_value(self, value):
+        pass
+
+    def remove_at_index(self, index):
+
+        count = 0
+
+        self.check_index_out_of_range(index)
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        current_node = self.head
+        while current_node:
+            if count == index - 1:
+                current_node.next = current_node.next.next
+                break
+            current_node = current_node.next
+            count += 1
+
+    def check_index_out_of_range(self, index):
+
+        assert index >= 0 and index <= self.size_of_list(), \
+            "(Error) Invalid index !!!"
---
+++ Python/linked_list/linked_list/singly_linked_list/singly_node.py
@@ -0,0 +1,4 @@
+class SinglyNode(object):
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
