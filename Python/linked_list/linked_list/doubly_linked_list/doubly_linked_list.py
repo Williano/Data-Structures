86a6a6b4ff3b04ce8a26484cf9bac78ef292a305
Williano <paawilly17@gmail.com>
2021-02-06 13:46:21 -0600

    Add method to search by value in sorted linked list

--- Python/linked_list/linked_list/doubly_linked_list/doubly_linked_list.py
+++ Python/linked_list/linked_list/doubly_linked_list/doubly_linked_list.py
@@ -20,3 +20,35 @@ class DoublyLinkedList(object):
         while current_node:
             print(current_node.data, end=" <--- ")
             current_node = current_node.previous
+
+    def search_by_value(self, value):
+
+        recent_search_result_pointer = self.head
+
+        if self.head is None:
+            print("List is empty! You can search through an empty list.")
+            return False
+        elif recent_search_result_pointer is None:
+            recent_search_result_pointer = self.head
+
+        # If the the value comes before the recent result pointer,
+        # we traverse backward otherwise traverse forward.
+        if value < recent_search_result_pointer.data:
+            while recent_search_result_pointer and value <= \
+                    recent_search_result_pointer.data:
+                if value == recent_search_result_pointer.data:
+                    return True
+                else:
+                    recent_search_result_pointer = \
+                        recent_search_result_pointer.previous
+        else:
+            while recent_search_result_pointer and value >= \
+                    recent_search_result_pointer.data:
+                if value == recent_search_result_pointer.data:
+                    return True
+                else:
+                    recent_search_result_pointer = \
+                        recent_search_result_pointer.next
+
+        # If the target is not found in the list, return False
+        return False
