ef82457e7513c03e681fa3b63b6df4e6c3908600
Williano <paawilly17@gmail.com>
2021-02-06 13:25:51 -0600

    Add reverse traversal method

--- Python/linked_list/linked_list/doubly_linked_list/doubly_linked_list.py
+++ Python/linked_list/linked_list/doubly_linked_list/doubly_linked_list.py
@@ -4,4 +4,12 @@ from .doubly_node import DoublyNode
 class DoublyLinkedList(object):
 
     def __init__(self):
-        pass
+        self.head = None
+        self.tail = None
+
+    def reverse_traversal(self):
+        current_node = self.tail
+
+        while current_node:
+            print(current_node.data, end=" <--- ")
+            current_node = current_node.previous
