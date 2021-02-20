a047f794c4e062faabd60a6b67ccfb9f0d91902f
Williano <paawilly17@gmail.com>
2021-02-20 14:10:54 -0600

    Update

--- Python/linked_list/doubly_linked_list_example.py
+++ Python/linked_list/doubly_linked_list_example.py
@@ -2,7 +2,26 @@ from linked_list.doubly_linked_list.doubly_linked_list import DoublyLinkedList
 
 
 def main():
-    pass
+
+    doubly_list = DoublyLinkedList()
+
+    # insert values
+    doubly_list.insert_values(["banana", "mango", "grapes", "orange"])
+
+    # Traverse
+    # doubly_list.reverse_traversal()
+    # doubly_list.front_traversal()
+
+    # Insert at end
+    doubly_list.insert_at_end(data="figs")
+    doubly_list.front_traversal()
+    print(" ")
+
+    # Insert at index
+    doubly_list.insert_at_index(0, data="jackfruit")
+    doubly_list.front_traversal()
+    print(" ")
+    print(doubly_list.get_size_of_list())
 
 
 if __name__ == '__main__':
