9ee74f0729b17832beeb8717650c5c687f6e818d
Williano <paawilly17@gmail.com>
2022-01-31 23:25:33 -0600

    Update

--- Python/OOP/credit_card.py
+++ Python/OOP/credit_card.py
@@ -1,6 +1,6 @@
 class CreditCard:
     """
-     A consumer credit card.
+    A consumer credit card.
     """
 
     def __init__(self, customer, bank, account, limit):
@@ -21,73 +21,73 @@ class CreditCard:
         self._limit = limit
         self._balance = 0
 
-        def get_customer(self):
-            """
-             Return name of the customer.
+    def get_customer(self):
+        """
+         Return name of the customer.
 
-            :param self: references the instance of the class.
-            :return: customer: the customer name.
-            """
-            return self._customer
+        :param self: references the instance of the class.
+        :return: customer: the customer name.
+        """
+        return self._customer
 
-        def get_bank(self):
-            """
-            Return the bank's name.
+    def get_bank(self):
+        """
+        Return the bank's name.
 
-            :param self: references the instance of the class.
-            :return: bank: the name of the bank.
-            """
-            return self._bank
+        :param self: references the instance of the class.
+        :return: bank: the name of the bank.
+        """
+        return self._bank
 
-        def get_account(self):
-            """
-            Return the card identifying number (typically stored as a string).
+    def get_account(self):
+        """
+        Return the card identifying number (typically stored as a string).
 
-            :param self: references the instance of the class.
-            :return: account: the account identifier.
-            """
-            return self._account
+        :param self: references the instance of the class.
+        :return: account: the account identifier.
+        """
+        return self._account
 
-        def get_limit(self):
-            """
-            Return current credit limit.
+    def get_limit(self):
+        """
+        Return current credit limit.
 
-            :param self: references the instance of the class.
-            :return: limit: the credit limit.
-            """
-            return self._limit
+        :param self: references the instance of the class.
+        :return: limit: the credit limit.
+        """
+        return self._limit
 
-        def get_balance(self):
-            """
-            Return current balance.
+    def get_balance(self):
+        """
+        Return current balance.
 
-            :param self: references the instance of the class.
-            :return: balance: the current balance of the account.
-            """
-            return self._balance
+        :param self: references the instance of the class.
+        :return: balance: the current balance of the account.
+        """
+        return self._balance
 
-        def get_charge(self, price):
-            """
-            Charge given price to the card, assuming sufficient credit limit.
-            Return True if charge was processed; False if charge was denied.
+    def get_charge(self, price):
+        """
+        Charge given price to the card, assuming sufficient credit limit.
+        Return True if charge was processed; False if charge was denied.
 
-            :param self: references the instance of the class.
-            :param price: the price of the item.
-            :return: bool: True if charge was processed;
-                           False if charge was denied.
-            """
-            if price + self._balance > self._limit:
-                return False
-            else:
-                self._balance += price
-                return True
+        :param self: references the instance of the class.
+        :param price: the price of the item.
+        :return: bool: True if charge was processed;
+                       False if charge was denied.
+        """
+        if price + self._balance > self._limit:
+            return False
+        else:
+            self._balance += price
+            return True
 
-        def make_payment(self, amount):
-            """
-            Process customer payment that reduces balance.
+    def make_payment(self, amount):
+        """
+        Process customer payment that reduces balance.
 
-            :param self: references the instance of the class.
-            :param amount: the amount to be charged.
-            :return: balance: the balance of customer after deducting charge.
-            """
-            self._balance -= amount
+        :param self: references the instance of the class.
+        :param amount: the amount to be charged.
+        :return: balance: the balance of customer after deducting charge.
+        """
+        self._balance -= amount
