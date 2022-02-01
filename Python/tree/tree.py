c66cde437bb9974d5c7a62981058bbb702c490d9
Williano <paawilly17@gmail.com>
2022-01-31 21:19:01 -0600

    Update

--- Python/tree/tree.py
+++ Python/tree/tree.py
@@ -11,8 +11,24 @@ class TreeNode:
         child.parent = self
         self.children.append(child)
 
-    def print_tree(self) -> None:
-        print(self.data)
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
 
-        for child in self.children:
-            print(child.data)
+        return level
+
+    def print_tree(self) -> None:
+
+        spaces = " " * self.get_level() * 3
+
+        prefix = spaces + "|__" if self.parent else ""
+
+        print(prefix + self.data)
+
+        if self.children:
+
+            for child in self.children:
+                child.print_tree()
